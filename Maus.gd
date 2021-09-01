83094db36df2daebbc5602f6cb68fb5034e5888c
DESKTOP-G7KNU2I\kijub <Kijubei@gmail.com>
2021-09-01 15:45:26 +0200

    * vorbereitung AI (Maus kann jetzt alles empfangen) * Regal szene gebaut und einmal in level gesetzt

--- Maus.gd
+++ Maus.gd
@@ -2,5 +2,22 @@ extends KinematicBody
 
 signal mouseCaught
 
+var player: KinematicBody = null
+
+onready var kiteAI = preload("res://Scenes/KiteAI.gd").new()
+
+func _ready():
+	kiteAI._ready()
+
+func _process(delta):
+	if player:
+		print("x " + str(player.global_transform.origin.x))
+		print("y " + str(player.global_transform.origin.y))
+		
+		
+
 func _on_CatchArea_body_entered(_body):
 	emit_signal("mouseCaught")
+
+func _on_PlayerDetectionArea_body_entered(body):
+	player = body
