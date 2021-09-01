c1c343fa081e01356900e3d70554018cacfd11f4
DESKTOP-G7KNU2I\kijub <Kijubei@gmail.com>
2021-09-01 14:06:45 +0200

    refactoring

--- Scenes/Mimi.gd
+++ Scenes/Mimi.gd
@@ -17,8 +17,9 @@ enum {
 
 const maxSpeed = 100
 const gravity = 0.98
-const hLookSensibility = 1.0
-const vLookSensibility = 1.0
+const terminalVelocity = -25
+const hLookSensibility = 0.2
+const vLookSensibility = 0.2
 
 onready var cam = $CamBase
 onready var energyBar = $EnergyBar
@@ -27,7 +28,6 @@ onready var animationPlayer = $MimiAnimation/AnimationPlayer
 
 var state = walk
 var velocity = Vector3.ZERO
-var isPouncing = false
 var grounded = false
 var exhausted = false
 var disabled = false
@@ -43,6 +43,9 @@ func _physics_process(delta):
 		return
 	grounded = is_on_floor()
 	
+	if grounded:
+		velocity.y = 0
+	
 	match state:
 		walk:
 			walkState(delta)
@@ -57,56 +60,50 @@ func _physics_process(delta):
 ########## WALK ##########
 
 func walkState(_delta):
-	var inputVector = applyInput()
-	
-	if inputVector != Vector3.ZERO:
-		velocity = velocity.move_toward(inputVector * walkSpeed, acceleration)
-	else:
-		velocity = velocity.move_toward(Vector3(0,-1,0), friction)
-		
+	applyInput(walkSpeed)
+	applyWalkAnimation()
+	moveByVelocity()
+	switchStateFromWalk()
+	restoreEnergyForWalk()
+
+func applyWalkAnimation():
 	if !(jumpAnimation() and animationPlayer.is_playing()):
 		if velocity.x != 0 or velocity.z != 0:
 			animationPlayer.play("Cat_walk")
 		else:
-			# doesnt work
 			animationPlayer.play("Cat_idle")
 
-	moveByVelocity()
-		
+func switchStateFromWalk():
 	if grounded and Input.is_action_pressed("sprint") and fitForSprint():
 		state = sprint
 	if grounded and Input.is_action_pressed("pounce") and fitForPounce():
 		state = pounce
 	if grounded and Input.is_action_pressed("jump") and fitForJump():
 		state = jump
-		
+
+func restoreEnergyForWalk():
 	if energyBar.value < energyBar.max_value and !exhausted:
 		energyBar.value += energyBar.step
 
 ########## SPRINT ##########
 
-func fitForSprint() -> bool:
-	return energyBar.value > 5
-
 func sprintState(_delta):
-	var inputVector = applyInput()
-	
-	if inputVector != Vector3.ZERO:
-		velocity = velocity.move_toward(inputVector * sprintSpeed, acceleration)
-	else:
-		velocity = velocity.move_toward(Vector3.ZERO, friction)
-
+	applyInput(sprintSpeed)
 	animationPlayer.play("Cat_run")
-
 	moveByVelocity()
-		
+	switchStateFromSprint()
+	consumeEnergyForSprint()
+
+
+func switchStateFromSprint():
 	if grounded and Input.is_action_just_released("sprint"):
 		state = walk
 	if grounded and Input.is_action_pressed("pounce") and fitForPounce() :
 		state = pounce
 	if grounded and Input.is_action_pressed("jump") and fitForJump():
 		state = jump
-		
+
+func consumeEnergyForSprint():
 	if energyBar.value > energyBar.min_value:
 		energyBar.value -= energyBar.step
 	else:
@@ -114,66 +111,51 @@ func sprintState(_delta):
 		rest()
 		state = walk
 
+func fitForSprint() -> bool:
+	return energyBar.value > 5
+
 ########## POUNCE ##########
+
+func pounceState(_delta):
+	applyPounce()
+	holdJumpAnimation()
+	moveByVelocity()
+	stopStateOnGround()
+
+func applyPounce():
+	if grounded:
+		animationPlayer.play("Cat_jump")
+		velocity *= pounceSpeedMultiplier
+		velocity.y += pounceJumpForce
+		velocity.z = clamp(velocity.z, -maxSpeed, maxSpeed)
+		velocity.x = clamp(velocity.x, -maxSpeed, maxSpeed)
+		velocity = move_and_slide(velocity, Vector3(0,1,0))
+		energyBar.value -= energyBar.step*200
+		return
 
 func fitForPounce() -> bool:
 	return (energyBar.value - energyBar.step*200) > energyBar.min_value
 
-func pounceState(_delta):
-	if !isPouncing:
-		applyPounce()
-	
-	moveByVelocity()
-	
-	if animationPlayer.current_animation_position > 0.4:
-		animationPlayer.stop(false)
-	
-	# needed somehow ...
-	grounded = is_on_floor()
-	
-	if grounded:
-		animationPlayer.play()
-		state = walk
-		isPouncing = false
-		
-
-func applyPounce():
-	isPouncing = true
-	animationPlayer.play("Cat_jump")
-	velocity *= pounceSpeedMultiplier
-	velocity.y += pounceJumpForce
-	velocity.z = clamp(velocity.z, -maxSpeed, maxSpeed)
-	velocity.x = clamp(velocity.x, -maxSpeed, maxSpeed)
-	velocity = move_and_slide(velocity, Vector3(0,1,0))
-	energyBar.value -= energyBar.step*200
-	return
-
 ########## JUMP ##########
 
-func fitForJump() -> bool:
-	return (energyBar.value - energyBar.step*200) > energyBar.min_value
-
 func jumpState(_delta):
+	applyJump()
+	holdJumpAnimation()
+	moveByVelocity()
+	stopStateOnGround()
+
+func applyJump():
 	if grounded:
 		velocity.y += jumpForce
 		energyBar.value -= energyBar.step*200
 		animationPlayer.play("Cat_jump")
 
-	if animationPlayer.current_animation_position > 0.4:
-		animationPlayer.stop(false)
-		
-	moveByVelocity()
-	
-	# needed somehow ...
-	grounded = is_on_floor()
-	
-	if grounded:
-		animationPlayer.play()
-		state = walk
+func fitForJump() -> bool:
+	return (energyBar.value - energyBar.step*200) > energyBar.min_value
 
 ########## HELPER FUNC ##########
 
-func applyInput() -> Vector3:
+func input() -> Vector3:
 	var inputVector = Vector3.ZERO
 	if Input.is_action_pressed("move_forward"):
 		inputVector.z -= 1
@@ -184,13 +166,35 @@ func applyInput() -> Vector3:
 	if Input.is_action_pressed("move_right"):
 		inputVector.x += 1
 	inputVector = inputVector.normalized()
+	inputVector.y = velocity.y
 	inputVector = inputVector.rotated(Vector3(0, 1, 0), rotation.y)
+	
 	return inputVector
+
+func applyInput(speedMultiplier):
+	var inputVector = input()
+	if inputVector != Vector3.ZERO:
+		velocity = velocity.move_toward(inputVector * speedMultiplier, acceleration)
+	else:
+		velocity = velocity.move_toward(Vector3(0,-1,0), friction)
 
 func moveByVelocity():
 	#bug: bei walk state ist gravity nicht richtig
 	velocity.y -= gravity
-	velocity = move_and_slide(velocity, Vector3(0,1,0))
+	velocity.y = clamp(velocity.y, terminalVelocity, 1000)
+	velocity = move_and_slide(velocity, Vector3.UP)
+
+func stopStateOnGround():
+	# needed somehow ...
+	grounded = is_on_floor()
+	
+	if grounded:
+		animationPlayer.play()
+		state = walk
+
+func holdJumpAnimation():
+	if animationPlayer.current_animation_position > 0.4:
+		animationPlayer.stop(false)
 
 func rest():
 	if exhausted:
