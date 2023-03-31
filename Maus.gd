c289070f3fa0ba5a9a01e8050e4b2efecc76c1e0
Kijubei <Kijubei@gmail.com>
2023-03-31 09:32:48 +0200

    fixes?

--- Maus.gd
+++ Maus.gd
@@ -2,22 +2,35 @@ extends KinematicBody
 
 signal mouseCaught
 
-var player: KinematicBody = null
+var playerInRange: KinematicBody = null
+
+export var targetPath = NodePath()
+export var speed = 10
 
 onready var kiteAI = preload("res://Scenes/KiteAI.gd").new()
 
+onready var agent: Navigation = $MausAgent
+onready var targetNode = get_node(targetPath)
+
 func _ready():
 	kiteAI._ready()
-
-func _process(delta):
-	if player:
-		print("x " + str(player.global_transform.origin.x))
-		print("y " + str(player.global_transform.origin.y))
+	if targetPath != null:
+		print("yes")
+	#	agent.set_target_location(targetNode.)
 		
-		
+#func _physics_process(delta):
+#	if agent.is_navigation_finished():
+#		return
+	
+#	var next: Vector3 = agent.get_next_location()
+#	var velocity = (next - transform.origin).normalized() * speed * delta
+#	velocity = velocity.move_toward((next - transform.origin).normalized() * speed * delta, 20)
+#	move_and_collide(velocity)
+#	Maus ist falsch rum (?)
+#	look_at(next, Vector3.UP)
 
 func _on_CatchArea_body_entered(_body):
 	emit_signal("mouseCaught")
 
 func _on_PlayerDetectionArea_body_entered(body):
-	player = body
+	playerInRange = body
